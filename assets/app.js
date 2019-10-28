675775ed3cfe4df30d50f19740133bc041764320
Sarah Naas <sjnaas@gmail.com>
2019-10-28 14:17:47 -0700

    created files for project

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBSdPGhZogrV4wPpmiTyBRnJmL-LIeC78Q",
+  authDomain: "train-scheduler-d7046.firebaseapp.com",
+  databaseURL: "https://train-scheduler-d7046.firebaseio.com",
+  projectId: "train-scheduler-d7046",
+  storageBucket: "train-scheduler-d7046.appspot.com",
+  messagingSenderId: "119491307948",
+  appId: "1:119491307948:web:269f7bf035b016d1887209"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
